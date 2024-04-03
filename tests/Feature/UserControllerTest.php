099a74353f9f6631e938ea7f9732cdd920a235e4
Takku <farelvivo06@gmail.com>
2024-04-04 01:45:19 +0700

    menambah action login dan logout

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -14,4 +14,37 @@ class UserControllerTest extends TestCase
         $this->get('/login')
         ->assertSeeText("Login");
     }
+
+    public function testLoginSuccess()
+    {
+        $this->post('/login', [
+            "user" => "farel",
+            "password" => "rahasia"
+        ])->assertRedirect("/")
+            ->assertSessionHas("user", "farel");
+    }
+
+    public function testLoginValidationError()
+    {
+        $this->post("/login", [])
+            ->assertSeeText("User or password is required");
+    }
+
+    public function testLoginFailed()
+    {
+        $this->post('/login', [
+            'user' => "wrong",
+            "password" => "wrong"
+        ])->assertSeeText("User or password is wrong");
+    }
+
+    public function testLogout()
+    {
+        $this->withSession([
+            "user" => "farel"
+        ])->post('/logout')
+            ->assertRedirect("/")
+            ->assertSessionMissing("user");
+    }
+
 }
